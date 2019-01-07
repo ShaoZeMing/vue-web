9599b82a8cdae7b4de4a7e8f73efc678f8812d1d
shaozeming <szm19920426@gmail.com>
2019-01-07 22:59:34 +0800

    调整js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
-import 'element-ui/lib/theme-chalk/display.css';
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import '../element-variables.scss'
+import 'element-ui/lib/theme-chalk/display.css';
 
 Vue.use(Element)
